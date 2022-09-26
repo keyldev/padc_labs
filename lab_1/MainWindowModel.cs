216227f2f6c9e8d09f567edda7ab993e53205b65
Vasiliy Zalatov <55500552+keyldev@users.noreply.github.com>
2022-09-26 12:01:19 +0300

    Add files via upload

---
+++ lab_1/MainWindowModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class MainWindowModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private string? _resultText;
+
+        public string ResultText
+        {
+            get { return _resultText ?? "null"; }
+            set { _resultText = value; NotifyPropertyChanged(); }
+        }
+        public RelayCommand ResumeZero { get; set; }
+        public RelayCommand ResumeFirst { get; set; }
+        public RelayCommand SuspendZero { get; set; }
+        public RelayCommand SuspendFirst { get; set; }
+        public RelayCommand ShowResult { get; set; }
+        private int counter = 0;
+
+        public MainWindowModel()
+        {
+            Thread thread_1 = new Thread(func_1);
+            Thread thread_2 = new Thread(func_2);
+
+            thread_1.Start();
+            thread_2.Start();
+
+            ResumeZero = new RelayCommand(o =>
+            {
+                thread_1.Resume();
+            });
+            ResumeFirst = new RelayCommand(o =>
+            {
+                thread_2.Resume();
+            });
+            SuspendZero = new RelayCommand(o =>
+            {
+                thread_1.Suspend();
+            });
+            SuspendFirst = new RelayCommand(o =>
+            {
+                thread_2.Suspend();
+            });
+            ShowResult = new RelayCommand(o =>
+            {
+                ResultText = counter.ToString();
+            });
+
+        }
+        private void func_1(object? obj)
+        {
+            for (; ; )
+            {
+                counter++;
+                ResultText = counter.ToString();
+            }
+        }
+        private void func_2(object? obj)
+        {
+            for (; ; )
+            {
+
+                counter--;
+                ResultText = counter.ToString();
+            }
+        }
+
+
+    }
+}
